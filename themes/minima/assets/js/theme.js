ab56f3610e02e7919e4a74510095e0f242e65829
juzhiyuan <juzhiyuan@apache.org>
2022-03-27 14:41:17 +0800

    chore: remove default switcher

--- themes/minima/assets/js/theme.js
+++ themes/minima/assets/js/theme.js
@@ -1,12 +1,7 @@
-const icon_light = '{{ index .Site.Params.switch 1 }}'
-const icon_dark = '{{ index .Site.Params.switch 0 }}'
 const comment = '{{ .Site.Params.comment }}'
 const light = 'light', dark = 'dark'
 
 const themeSwitcher = document.getElementById('theme-switcher')
-
-// set switcher
-themeSwitcher.innerHTML = localStorage.theme === light ? icon_light : icon_dark
 
 themeSwitcher.addEventListener('click', function () {
   const currentTheme = localStorage.theme
@@ -22,12 +17,10 @@ themeSwitcher.addEventListener('click', function () {
 
 function switchMinimaTheme(oldTheme, newTheme) {
   const { classList } = document.documentElement
-  const text = newTheme === light ? icon_light : icon_dark;
-  
+
   classList.remove(oldTheme);
   classList.add(newTheme);
   localStorage.theme = newTheme;
-  themeSwitcher.innerHTML = text;
 }
 
 const utteranceClassName = '.utterances-frame'
